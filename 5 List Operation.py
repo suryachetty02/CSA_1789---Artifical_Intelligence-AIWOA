133bb493159e2b817eb9f45df3c454d068572df8
Surya Chetty <113962663+suryachetty02@users.noreply.github.com>
2023-02-09 20:18:13 +0530

    Add files via upload

---
+++ 5 List Operation.py
@@ -0,0 +1,10 @@
+print("List operation")
+l1=[1,2,3,4]
+l2=[5,6,7,8]
+print("Nested list:",[l1,l2])
+print("Length of  list 1:",len(l1))
+print("Concatenation",l1+l2)
+print("Membership of 1 in list 1: ",(1 in l1))
+print("Iteration: ",l1*3)
+print("3rd index element in l1: ",l1[3])
+print("Slicing: ",l1[2:])
