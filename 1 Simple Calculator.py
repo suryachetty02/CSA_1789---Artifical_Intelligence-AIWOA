65c839c13dace610e505cfb058c361b59116efbe
Surya Chetty <113962663+suryachetty02@users.noreply.github.com>
2023-02-09 20:16:53 +0530

    Add files via upload

---
+++ 1 Simple Calculator.py
@@ -0,0 +1,11 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=input("Enter your choice of operation:")
+if (c=="+"):
+    print("Sum is:", a+b)
+elif(c=="-"):
+    print("Difference is:", a-b)
+elif(c=="*"):
+    print("Product is:", a*b)
+elif(c=="/"):
+    print("Division is:",a/b)
