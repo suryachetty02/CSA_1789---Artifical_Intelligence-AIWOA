974f9307125ddc776f09f99d2a78ba16efe76e9a
Surya Chetty <113962663+suryachetty02@users.noreply.github.com>
2023-02-10 23:53:05 +0530

    Add files via upload

---
+++ 24 Implement Decision Tree.py
@@ -0,0 +1,15 @@
+from sklearn import tree
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(iris.data, iris.target)
+
+import graphviz 
+dot_data = tree.export_graphviz(clf, out_file=None, 
+                     feature_names=iris.feature_names,  
+                     class_names=iris.target_names,  
+                     filled=True, rounded=True,  
+                     special_characters=True)  
+graph = graphviz.Source(dot_data)  
+graph.render("iris")
