81371fa409d4e51ce1818692df5049c12c5aeb64
Surya Chetty <113962663+suryachetty02@users.noreply.github.com>
2023-02-11 09:32:57 +0530

    Update 2 Add Two Matrices.py

--- 2 Add Two Matrices.py
+++ 2 Add Two Matrices.py
@@ -1,14 +1,37 @@
-X = [[12,7,3],
-    [4 ,5,6],
-    [7 ,8,9]]
-Y = [[5,8,1],
-    [6,7,3],
-    [4,5,9]]
-result = [[0,0,0],
-         [0,0,0],
-         [0,0,0]]
-for i in range(len(X)):
-   for j in range(len(X[0])):
-       result[i][j] = X[i][j] + Y[i][j]
-for r in result:
-   print(r)
+print("Enter Row and Column Size of First Matrix: ", end="")
+rowOne = int(input())
+colOne = int(input())
+print("Enter Row and Column Size of Second Matrix: ", end="")
+rowTwo = int(input())
+colTwo = int(input())
+
+if rowOne==rowTwo and colOne==colTwo:
+    matOne = []
+    print("\nEnter " +str(rowOne*colOne)+ " Elements for First Matrix: ")
+    for i in range(rowOne):
+        matOne.append([])
+        for j in range(colOne):
+            num = int(input())
+            matOne[i].append(num)
+
+    matTwo = []
+    print("\nEnter " +str(rowTwo*colTwo)+ " Elements for Second Matrix: ")
+    for i in range(rowTwo):
+        matTwo.append([])
+        for j in range(colTwo):
+            num = int(input())
+            matTwo[i].append(num)
+
+    matThree = []
+    for i in range(rowOne):
+        matThree.append([])
+        for j in range(colTwo):
+            matThree[i].append(matOne[i][j]+matTwo[i][j])
+
+    print("\nAddition Result:")
+    for i in range(rowOne):
+        for j in range(colOne):
+            print(matThree[i][j], end=" ")
+        print()
+else:
+    print("\nDimension Mismatched!")
